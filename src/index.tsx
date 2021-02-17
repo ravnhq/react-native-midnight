28de07f1764d8d139de143fff4da9c93a56f49f2
Manuel Sánchez <manuel@ravn.co>
2021-02-16 19:53:16 -0600

    chore: exposes methods on the MidnightType

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { NativeModules } from 'react-native'
 
 type MidnightType = {
   multiply(a: number, b: number): Promise<number>
+  subscribe(): void
+  unsubscribe(): void
+  postNotification(): void
 }
 
 const { Midnight } = NativeModules
