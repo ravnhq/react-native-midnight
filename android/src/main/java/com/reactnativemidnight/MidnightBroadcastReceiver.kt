f2e4f7efd3617222e1a5cd1376fe376243b0fa0a
Lisandro Lopez <lisoflores@gmail.com>
2021-03-09 11:32:32 -0600

    Adding connection between BradcastReceiver and Midnight React module.

--- android/src/main/java/com/reactnativemidnight/MidnightBroadcastReceiver.kt
+++ android/src/main/java/com/reactnativemidnight/MidnightBroadcastReceiver.kt
@@ -4,27 +4,25 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.support.v4.content.LocalBroadcastManager
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class MidnightListener : BroadcastReceiver() {
+class MidnightBroadcastReceiver : BroadcastReceiver() {
 
-    private var date = Date()
-    private val dateFormat by lazy { SimpleDateFormat("yyMMdd", Locale.getDefault()) }
+
 
     override fun onReceive(context: Context, intent: Intent) {
         val action = intent.action
 
         val currentDate = Date()
 
-        if ((action == Intent.ACTION_TIME_CHANGED || action == Intent.ACTION_TIMEZONE_CHANGED) && !isSameDay(currentDate)) {
-            date = currentDate
-            MidnightModule.onDayChanged()
+        if ((action == Intent.ACTION_TIME_CHANGED || action == Intent.ACTION_TIMEZONE_CHANGED) /*&& !isSameDay(currentDate)*/) {
+          val broadcastIntent = Intent(MidnightModule.DAY_CHANGED_ACTION)
+          LocalBroadcastManager.getInstance(context).sendBroadcast(broadcastIntent)
         }
     }
-
-    private fun isSameDay(currentDate: Date) = dateFormat.format(currentDate) == dateFormat.format(date)
 
     companion object {
 
--- android/src/main/java/com/reactnativemidnight/MidnightModule.kt
+++ android/src/main/java/com/reactnativemidnight/MidnightModule.kt
@@ -1,33 +1,60 @@
 package com.reactnativemidnight
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.support.v4.content.LocalBroadcastManager
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MidnightModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    init {
-        midnightModuleReactContext = reactContext
+class MidnightModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+  private var date = Date()
+  private val dateFormat by lazy { SimpleDateFormat("yyMMdd", Locale.getDefault()) }
+
+  private val localBroadcastManager = LocalBroadcastManager.getInstance(reactContext)
+  private val midnightBroadcastReceiver = MidnightBroadcastReceiver()
+  private var midnightReceiver: BroadcastReceiver? = object : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+      sendEvent("Hi there!")
     }
 
-    override fun getName(): String {
-        return "Midnight"
+  }
+
+  override fun getName(): String {
+    return "Midnight"
+  }
+
+  fun subscribe() {
+    reactApplicationContext.registerReceiver(midnightBroadcastReceiver, MidnightBroadcastReceiver.getIntentFilter())
+    midnightReceiver?.let {
+      localBroadcastManager.registerReceiver(midnightReceiver, IntentFilter(DAY_CHANGED_ACTION))
     }
+  }
 
-    fun manuallyTriggerEvent() {
-        MidnightModule.onDayChanged()
-    }
+  fun unsubscribe() {
+    reactApplicationContext.unregisterReceiver(midnightBroadcastReceiver)
+    localBroadcastManager.unregisterReceiver(midnightReceiver)
+    midnightReceiver = null
+  }
 
-    companion object {
-        private var midnightModuleReactContext: ReactApplicationContext ? = null
+  fun manuallyTriggerEvent() {
+    sendEvent("HI")
+  }
 
-        private fun sendEvent(eventName: String, data: Any?) {
-            val reactApplicationContext = midnightModuleReactContext ?: return
-            reactApplicationContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(eventName, data)
-        }
+  private fun sendEvent(data: String) {
+    reactContext.getJSModule(RCTDeviceEventEmitter::class.java).emit("midnight", data)
+  }
 
-        fun onDayChanged() {
-            sendEvent("dayChanged", null)
-        }
-    }
+  private fun isSameDay(currentDate: Date) = dateFormat.format(currentDate) == dateFormat.format(date)
+
+  companion object {
+    const val DAY_CHANGED_ACTION = "day_changed_action"
+  }
+
 }
