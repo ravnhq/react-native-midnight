bec686ef0e43d3dc8aa2d11aae316fac06f0af85
Donovan Hiland <donovan.hiland@gmail.com>
2021-03-12 10:57:17 -0700

    refactor: missing module error and index file ext (#7)

--- src/NativeMidnight.ts
+++ src/NativeMidnight.ts
@@ -13,11 +13,9 @@ if (Platform.OS !== 'ios' && Platform.OS !== 'android') {
 
 if (!NativeMidnight) {
   // Produce an error if we don't have the native module
-  throw new Error(`react-native-midnight: NativeModule.Midnight is null. To fix this issue try these steps:
-  • For react-native <= 0.59: Run \`react-native link react-native-midnight\` in the project root.
-  • Rebuild and re-run the app.
-  • If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
-  If none of these fix the issue, please open an issue on the Github repository: https://github.com/ravnhq/react-native-midnight`)
+  throw new Error(
+    `react-native-midnight: NativeModule.Midnight is null. Make sure you have the package linked properly.`,
+  )
 }
 
 export { NativeMidnight }
